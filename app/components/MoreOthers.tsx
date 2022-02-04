cb7cf7aa90c29a361a313428d592fce73fc58db2
xc17 <xaliscoral@gmail.com>
2022-02-04 21:43:48 +0700

    Fix MoreOthers Component Bug

--- app/components/MoreOthers.tsx
+++ app/components/MoreOthers.tsx
@@ -1,17 +1,18 @@
-import { Link } from "remix";
+import { Link } from 'remix';
 
-export default function MoreOthers(data: any) {
-    return (
-        <div className="more-others">
-            <h2>OTHER STUFF</h2>
-            <hr />
-            <div className="other-subcontainer">
-                {data && data.map((x: any) => (
-                    <Link key={x.url} to={x.url}>
-                        {x.name.toLowerCase().replace(/ /g, "_")}
-                    </Link>
-                ))}
-            </div>
-        </div>
-    );
+export default function MoreOthers({ data }: any) {
+	return (
+		<div className='more-others'>
+			<h2>OTHER STUFF</h2>
+			<hr />
+			<div className='other-subcontainer'>
+				{data &&
+					data.map((x: any) => (
+						<Link key={x.url} to={x.url}>
+							{x.name.toLowerCase().replace(/ /g, '_')}
+						</Link>
+					))}
+			</div>
+		</div>
+	);
 }
