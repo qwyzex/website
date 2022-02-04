a7589bf23313a5d99e6f8e9f992014bc5e704056
xc17 <xaliscoral@gmail.com>
2022-02-05 02:30:54 +0700

    Completely custom RouteNav href

--- app/components/RouteNav.tsx
+++ app/components/RouteNav.tsx
@@ -1,50 +1,38 @@
-import { Link } from "remix";
+import { Link } from 'remix';
 
 export interface RouteNavProps {
-    lvl?: number;
-    index?: boolean | string;
-    custom?: any;
-    text: string | Array<string>;
+	lvl?: number;
+	href: string | string[];
+	text: string | Array<string>;
 }
 
 export default function RouteNav(props: RouteNavProps) {
-    if (props.lvl === 3) {
-        return <RouteNavThree index={props.index} text={props.text} />;
-    } else {
-        return (
-            <RouteNavTwo index={props.index} custom={props.custom} text={props.text} />
-        );
-    }
+	if (props.lvl === 3) {
+		return <RouteNavThree href={props.href} text={props.text} />;
+	} else {
+		return <RouteNavTwo href={props.href} text={props.text} />;
+	}
 }
 
 export const RouteNavTwo = (props: RouteNavProps) => {
-    return (
-        <div className="route-nav">
-            <Link to="/">Home</Link>
-            &#9656;
-            <a
-                href={
-                    props.index === true
-                        ? ".."
-                        : props.index === "custom"
-                        ? props.custom
-                        : "."
-                }
-            >
-                {props.text}
-            </a>
-        </div>
-    );
+	return (
+		<div className='route-nav'>
+			<Link to='/'>Home</Link>
+			&#9656;
+			{/* @ts-ignore */}
+			<Link to={props.href}>{props.text}</Link>
+		</div>
+	);
 };
 
 export const RouteNavThree = (props: RouteNavProps) => {
-    return (
-        <div className="route-nav">
-            <Link to="/">HOME</Link>
-            &#9656;
-            <a href={props.index === true ? ".." : "."}>{props.text[0]}</a>
-            &#9656;
-            <a href={props.index === true ? "." : ".."}>{props.text[1]}</a>
-        </div>
-    );
+	return (
+		<div className='route-nav'>
+			<Link to='/'>HOME</Link>
+			&#9656;
+			<Link to={props.href[0]}>{props.text[0]}</Link>
+			&#9656;
+			<Link to={props.href[1]}>{props.text[1]}</Link>
+		</div>
+	);
 };
