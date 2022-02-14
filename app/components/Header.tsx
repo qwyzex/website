78f0723504ff9a571a3cc07ec719d9a04c1ec53a
qwyzex <qwyzex@gmail.com>
2022-02-14 15:43:04 +0700

    Imp: Header nav link active state

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,37 @@
-import { Link } from 'remix';
+import { Link, useLocation } from 'remix';
 import { useState } from 'react';
 import { MainLogo } from '../root';
 import { AdminHeaderLink } from '../routes/admin';
+import { Route, useRoutes } from 'react-router';
 
 const Header = () => {
 	const [openSidebar, setOpenSidebar] = useState(false);
+	const currentRoute = useLocation();
+
+	interface ListItemLinkProps {
+		to: string;
+		title: string;
+		regex: RegExp;
+	}
+
+	const ListItem = (props: ListItemLinkProps) => {
+		const match = currentRoute.pathname.match(props.regex);
+
+		return (
+			<li
+				className={`nav-items ${match ? 'active' : ''}`}
+				style={{
+					backgroundColor: currentRoute.pathname.match(props.regex)
+						? '#ff003c'
+						: '#00000000',
+				}}
+			>
+				<Link to={props.to}>
+					<p>{props.title}</p>
+				</Link>
+			</li>
+		);
+	};
 
 	return (
 		<header className='header' id='header'>
@@ -13,57 +40,25 @@ const Header = () => {
 			</Link>
 			<nav className={`navigation ${openSidebar ? 'open' : ''}`}>
 				<ul className='nav-list'>
-					<li className='nav-items'>
-						<Link to='/' className='essentials home'>
-							<object
-								data='/images/svg/home.svg'
-								type='image/svg+xml'
-							></object>
-							<p>HOME</p>
-						</Link>
-					</li>
-					<li className='nav-items'>
-						<Link to='/posts' className='essentials posts'>
-							<object
-								data='/images/svg/newspaper.svg'
-								type='image/svg+xml'
-							></object>
-							<p>POSTS</p>
-						</Link>
-					</li>
-					<li className='nav-items'>
-						<Link to='/gallery' className='essentials gallery'>
-							<object
-								data='/images/svg/bank.svg'
-								type='image/svg+xml'
-							></object>
-							<p>GALLERY</p>
-						</Link>
-					</li>
-					<li className='nav-items'>
-						<Link to='/profile' className='essentials profile'>
-							<object
-								data='/images/svg/user.svg'
-								type='image/svg+xml'
-							></object>
-							<p>PROFILE</p>
-						</Link>
-					</li>
-					<li className='nav-items'>
-						<Link to='/contact' className='essentials contact'>
-							<object
-								data='/images/svg/email.svg'
-								type='image/svg+xml'
-							></object>
-							<p>CONTACT</p>
-						</Link>
-					</li>
+					<ListItem regex={/^\/$/} to='/' title='Home' />
+					<ListItem regex={/^\/posts/} to='/posts' title='Posts' />
+					<ListItem
+						regex={/^\/gallery/}
+						to='/gallery'
+						title='Gallery'
+					/>
+					<ListItem
+						regex={/^\/profile/}
+						to='/profile'
+						title='Profile'
+					/>
+					<ListItem
+						regex={/^\/contact/}
+						to='/contact'
+						title='Contact'
+					/>
 					<AdminHeaderLink>
-						<Link to='/admin' className='essentials admin'>
-							<object
-								data='/images/svg/stars.svg'
-								type='image/svg+xml'
-							></object>
+						<Link to='/admin' className='admin'>
 							<p>ADMIN</p>
 						</Link>
 					</AdminHeaderLink>
