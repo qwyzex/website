94dd4ca6d5e0893ab3d979328ff6e725c048d4fe
qwyzex <qwyzex@gmail.com>
2022-08-13 21:37:25 +0700

    HOTFIX: header anchor styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,80 +1,80 @@
-import { Link, useLocation } from 'remix';
-import { useState } from 'react';
+// @ts-nocheck
+import { Link } from 'remix';
 import { MainLogo } from '../root';
 import { AdminHeaderLink } from '../routes/admin';
-import { Route, useRoutes } from 'react-router';
 
 const Header = () => {
-	const [openSidebar, setOpenSidebar] = useState(false);
-	const currentRoute = useLocation();
+	function mobileNavbar() {
+		let con = document.querySelector('.nav-button-cont');
+		let img = document.querySelector('.nav-button-image');
+		let nav = document.querySelector('.navigation');
 
-	interface ListItemLinkProps {
-		to: string;
-		title: string;
-		regex: RegExp;
+		con.classList.toggle('active');
+		img.classList.toggle('clicked');
+		nav.classList.toggle('open');
 	}
-
-	const ListItem = (props: ListItemLinkProps) => {
-		const match = currentRoute.pathname.match(props.regex);
-
-		return (
-			<li
-				className={`nav-items ${match ? 'active' : ''}`}
-				style={{
-					backgroundColor: currentRoute.pathname.match(props.regex)
-						? '#ff003c'
-						: '#00000000',
-				}}
-			>
-				<Link to={props.to}>
-					<p>{props.title}</p>
-				</Link>
-			</li>
-		);
-	};
 
 	return (
 		<header className='header' id='header'>
 			<Link to='/'>
 				<MainLogo />
 			</Link>
-			<nav className={`navigation ${openSidebar ? 'open' : ''}`}>
+			<nav className='navigation'>
 				<ul className='nav-list'>
-					<ListItem regex={/^\/$/} to='/' title='Home' />
-					<ListItem regex={/^\/posts/} to='/posts' title='Posts' />
-					<ListItem
-						regex={/^\/gallery/}
-						to='/gallery'
-						title='Gallery'
-					/>
-					<ListItem
-						regex={/^\/profile/}
-						to='/profile'
-						title='Profile'
-					/>
-					<ListItem
-						regex={/^\/contact/}
-						to='/contact'
-						title='Contact'
-					/>
-					<AdminHeaderLink>
-						<Link to='/admin' className='admin'>
-							<p>ADMIN</p>
+					<li className='nav-items'>
+						<Link to='/' className='essentials home'>
+							<object
+								data='/images/svg/home.svg'
+								type='image/svg+xml'
+							></object>
+							<p>HOME</p>
 						</Link>
-					</AdminHeaderLink>
+					</li>
+					<li className='nav-items'>
+						<Link to='/posts' className='essentials posts'>
+							<object
+								data='/images/svg/newspaper.svg'
+								type='image/svg+xml'
+							></object>
+							<p>POSTS</p>
+						</Link>
+					</li>
+					<li className='nav-items'>
+						<Link to='/gallery' className='essentials gallery'>
+							<object
+								data='/images/svg/bank.svg'
+								type='image/svg+xml'
+							></object>
+							<p>GALLERY</p>
+						</Link>
+					</li>
+					<li className='nav-items'>
+						<Link to='/profile' className='essentials profile'>
+							<object
+								data='/images/svg/user.svg'
+								type='image/svg+xml'
+							></object>
+							<p>PROFILE</p>
+						</Link>
+					</li>
+					<li className='nav-items'>
+						<Link to='/contact' className='essentials contact'>
+							<object
+								data='/images/svg/email.svg'
+								type='image/svg+xml'
+							></object>
+							<p>CONTACT</p>
+						</Link>
+					</li>
+					<AdminHeaderLink />
 				</ul>
 			</nav>
-			<div className={`nav-button-cont ${openSidebar ? 'active' : ''}`}>
-				<button
-					className='nav-button'
-					onClick={() => setOpenSidebar(!openSidebar)}
-				></button>
+			<div className='nav-button-cont'>
+				<button className='nav-button' onClick={mobileNavbar}></button>
 				<div className='nav-button-image-cont'>
 					<object
 						data='/images/svg/triangle.svg'
-						className={`nav-button-image ${
-							openSidebar ? 'clicked' : ''
-						}`}
+						className='nav-button-image'
 					></object>
 				</div>
 			</div>
